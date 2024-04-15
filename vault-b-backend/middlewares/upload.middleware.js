3abe24be0436b1393e35ad7f61142ba9bcf99432
Ankur Kumar <ak6550352@gmail.com>
2024-04-16 00:19:15 +0530

    server error fix13

--- vault-b-backend/middlewares/upload.middleware.js
+++ vault-b-backend/middlewares/upload.middleware.js
@@ -16,7 +16,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const storage = multer.diskStorage({
     destination:function (req,file,cb){
-        cb(null,path.join(__dirname, 'public', 'temp'))
+        cb(null,path.join(__dirname, 'static', 'temp'))
         if (fs.existsSync(directory))
         console.log(" folder exist",directory)
         else
