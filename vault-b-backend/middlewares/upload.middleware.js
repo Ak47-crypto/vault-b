144b9f0e074de627d7bc9cae069fc70f73654a9c
Ankur Kumar <ak6550352@gmail.com>
2024-04-15 23:02:59 +0530

    server error fix4

--- vault-b-backend/middlewares/upload.middleware.js
+++ vault-b-backend/middlewares/upload.middleware.js
@@ -1,5 +1,9 @@
 import multer from "multer";
-import path from 'path'
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const storage = multer.diskStorage({
     destination:function (req,file,cb){
         cb(null,path.join(__dirname, 'public', 'temp'))
